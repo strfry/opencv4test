78bc998f3293ef93e9246008eb3d6c800f317895
Jonathan Sieber <mail@strfry.org>
2019-10-03 23:12:39 +0200

    initial commit

--- opencv4test/main.cpp
+++ opencv4test/main.cpp
@@ -1,3 +1,4 @@
+
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
@@ -8,10 +9,9 @@ using namespace cv;
 using namespace std;
 int main()
 {
-  
-    cv::Mat image(128, 128, CV_8UC3);
     cv::Mat image2(128, 128, CV_8UC3);
-    image = imread( "/green3.png", IMREAD_COLOR ); // Read the file
+
+    auto image = imread( "green3.png", IMREAD_COLOR ); // Read the file
     if( image.empty() )                      // Check for invalid input
     {
         cout <<  "Could not open or find the image" << std::endl ;
@@ -19,18 +19,22 @@ int main()
     }
     namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
 
-    int x=0,y=0,xx=0,yy=0;
-    for(x=0;x<=127;x++){
-    //    for(y=0;y<=127;y++){
-            xx=x+1;
-            yy=y+1;
-            image2.at<cv::Vec3b>(x, y) = image.at<cv::Vec3b>(xx, y);
-            imshow( "Display window", image2 );                // Show our image inside it.
-            usleep(1000);
-        //if (x==127){x=0;};
-    //    }
-       
+
+      int xx=0,yy=0;
+    while (bool running = true) {
+        xx=(xx+33) % 128;
+        yy=(yy+33) % 128;
+        for(int x=0;x<=127;x++){
+            for(int y=0;y<=127;y++){
+                image2.at<cv::Vec3b>(x, y) = image.at<cv::Vec3b>(x + xx, y);
+                usleep(1000);
+            }
+        }
+        imshow( "Display window", image2 );                // Show our image inside it.
+                
     }
-    waitKey(0); // Wait for a keystroke in the window
-    return 0;
+    
+        std::cout << waitKey(33) << std::endl;
+
+    return 0; // Wait for a keystroke in the window
 }
